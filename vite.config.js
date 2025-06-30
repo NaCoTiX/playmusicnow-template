9a953dbfb6fd2706c137f0e94dad1c50f30eb06f
NaCoTiX <samcoggins@live.co.uk>
2025-06-30 20:58:18 +0000

    Add outDir to vite config for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -7,6 +6,13 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: ['9fe0977a-b4bf-48bf-8b13-6bcd9346a519-00-1e6x1v6l0g0b1.worf.replit.dev', 'localhost', '127.0.0.1']
+    allowedHosts: [
+      '9fe0977a-b4bf-48bf-8b13-6bcd9346a519-00-1e6x1v6l0g0b1.worf.replit.dev',
+      'localhost',
+      '127.0.0.1'
+    ]
+  },
+  build: {
+    outDir: 'dist'
   }
 })
